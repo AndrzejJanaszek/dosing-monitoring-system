1bed044993d55f8a50d9ecff713d4390d52b52e7
Andrzej Janaszek <janaszek.andrzej.szkola@gmail.com>
2025-07-30 20:46:09 +0200

    litte dosing sketch

---
+++ PROGRAM/main.py
@@ -0,0 +1,41 @@
+def handle_tank_state_change():
+    # colision check
+    # store data
+    # 
+    # if END save data -> saveEvent() /or/ saveDosage()
+    # end clear stored data
+    # if this is ONLY runing event than clear collision point list
+    
+    
+    pass
+
+# tank list
+# serialManager -> event
+# databaseManager -> for handling function
+def dosing_control_thread(serialManager, databaseManager):
+    # signal event wait
+    # read signal
+    # signal event clear
+
+    # for each tank check state change
+    # if state changed -> handle change
+    
+    pass
+
+def main():
+    # load config
+
+    # init serialManager
+
+    # init databaseManager
+
+    # run cycle thread
+
+    # run dosing thread
+    
+    
+    pass
+
+
+if __name__ == "__main__":
+    main()
